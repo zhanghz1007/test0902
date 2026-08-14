8707c7fd9874d4efb3e28a813113780850fad761
liuslcfzhng995@cmss.cmcc <h0KOhkuo8fJNP@cmdevops.com>
2026-08-14 03:02:43 +0000

    j5

--- src/test/java/org/example/test/PrintCLassNameTest.java
+++ src/test/java/org/example/test/PrintCLassNameTest.java
@@ -1,13 +1,11 @@
 package org.example.test;
 
-
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;  // JUnit 5的Test注解
 
 public class PrintCLassNameTest {
 
-    @Test
-    public void printClassName() {
+    @Test  // JUnit 5的@Test注解，不需要public
+    void printClassName() {  // JUnit 5的方法可以是package-private，不需要public
         PrintCLassName printCLassName = new PrintCLassName();
         printCLassName.printClassName();
 
